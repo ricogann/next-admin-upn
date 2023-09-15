bd0e916a1d100006fef9089a91b980c42ba89421
Alfian Dorif Murtadlo1234 <100700571+IndyJones123@users.noreply.github.com>
2023-09-15 23:29:21 +0700

    Harga

---
+++ src/pages/harga/create.tsx
@@ -0,0 +1,139 @@
+import { useState, ChangeEvent } from "react";
+import SideBar from "@/components/sidebar";
+import { Input } from "@/components/input";
+import { Textarea } from "@/components/textarea";
+import { InputFiles } from "@/components/input-files";
+import { Submit } from "@/components/submit-button";
+import { useRouter } from "next/router";
+
+export default function Create() {
+    const router = useRouter();
+
+    const [nama, setnama] = useState("");
+    const [harga, setharga] = useState("");
+
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        if (event.target.name === "nama") {
+            setnama(event.target.value);
+        } else if (event.target.name === "harga") {
+            setharga(event.target.value);
+        }
+    };
+
+    // const handleTextAreaChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    //     setDeskripsiFasilitas(event.target.value);
+    // };
+
+    // const handleFotoChange = (event: ChangeEvent<HTMLInputElement>) => {
+    //     const files = Array.from(event.target.files || []);
+
+    //     setFotoFasilitas([...fotoFasilitas, ...files]);
+    // };
+
+    const sendData = async () => {
+        if (
+            nama === "" ||
+            harga === "" 
+        ) {
+            alert("Mohon isi semua field!");
+            return;
+        } else {
+            const data = new FormData();
+            data.append("nama", nama);
+            data.append("alamat", harga);
+            const res = await addHarga(data);
+        }
+    };
+
+    async function addHarga(data: FormData) {
+        try {
+            console.log(data);
+            const res = await fetch("http://localhost:5000/api/harga/add", {
+                method: "POST",
+                body: data,
+            });
+
+            const resData = await res.json();
+
+            if (resData.status === true) {
+                router.push("/harga");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // console.log(
+    //     nama,
+    //     harga,
+    //     deskripsiFasilitas,
+    //     fotoFasilitas,
+    //     jamBuka,
+    //     jamTutup,
+    //     durasi,
+    //     bukaHari
+    // );
+    return (
+        <div className="flex">
+            <div className="">
+                <SideBar />
+            </div>
+            <div className="h-screen w-full p-10 flex bg-[#F7F8FA]">
+                <div className="p-5">
+                    <div className="flex flex-col items-start justify-center">
+                        <div className="flex flex-row items-start">
+                            <h1 className="text-[40px] font-bold mr-14">
+                                Form Add Harga Fasilitas
+                            </h1>
+                        </div>
+                        <h4 className="text-[12] font-regular mb-14 text-dark-whiteText">
+                            Form Tambah Harga Fasilitas By Admin
+                        </h4>
+                    </div>
+
+                    <div className="flex flex-row items-start mb-5 border-b border-[#E2E7EE]">
+                        <button>
+                            <h2
+                                className={`text-[18] font-regular mb-3 mr-14 font-bold border-b-2 border-[#FFA101]`}
+                            >
+                                Form Tambah Harga Fasilitas
+                            </h2>
+                        </button>
+                    </div>
+
+                    <div className=" overflow-hidden rounded-lg shadow-lg">
+                        <div className="flex flex-wrap overflow-hidden rounded-lg shadow-lg">
+                            <div className="flex flex-col p-4">
+                                <h1 className="px-4">Data Fasilitas</h1>
+                                <div className="flex flex-row p-4 gap-5">
+                                    <Input
+                                        name="nama"
+                                        type="text"
+                                        className="px-5 py-2 placeholder-gray-400 text-gray-700 relative  bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring focus:ring-indigo-200"
+                                        placeholder="Nama..."
+                                        onChange={handleInputChange}
+                                    />
+                                    <Input
+                                        name="harga"
+                                        type="number"
+                                        className="px-5 py-2 placeholder-gray-400 text-gray-700 relative  bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring focus:ring-indigo-200"
+                                        placeholder="Harga..."
+                                        onChange={handleInputChange}
+                                    />
+                                </div>
+
+
+                                <Submit
+                                    message="SUBMIT"
+                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg mx-4"
+                                    onClick={sendData}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/pages/harga/index.tsx
@@ -0,0 +1,173 @@
+import { useState, useEffect, MouseEventHandler } from "react";
+import SideBar from "@/components/sidebar";
+import { useRouter } from "next/router";
+import Image from "next/image";
+
+interface harga {
+    id: number;
+    id_fasilitas: number;
+    nama: string;
+    harga: number;
+}
+
+
+export default function harga() {
+    const router = useRouter();
+
+    const [dataharga, setDataharga] = useState<harga[]>([]);
+
+    const handlePage = (link: string) => {
+        router.push(link);
+    };
+
+    async function getDataharga() {
+        try {
+            const res = await fetch("http://localhost:5000/api/harga");
+            const data = await res.json();
+
+            return data;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const data = await getDataharga();
+
+                setDataharga(data.data);
+            } catch (error) {
+                console.error("error fetching data harga ", error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    const handleDelete = async (id: number) => {
+        try {
+            const res = await fetch(
+                `http://localhost:5000/api/harga/delete/${Number(id)}`,
+                {
+                    method: "DELETE",
+                }
+            );
+
+            const data = await res.json();
+
+            if (data.status === true) {
+                window.location.reload();
+            } else {
+                alert("Gagal menghapus data harga");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <div className="flex overflow-x-hidden">
+            <div className="">
+                <SideBar />
+            </div>
+            <div className="h-screen w-full p-10 flex bg-[#F7F8FA]">
+                <div className="p-5">
+                    <div className="flex flex-col items-start justify-center">
+                        <div className="flex flex-row items-start">
+                            <h1 className="text-[45px] font-bold mr-14">
+                                Harga
+                            </h1>
+                        </div>
+                        <h4 className="text-[12] font-regular mb-14 text-dark-whiteText">
+                            Tabel Data Harga
+                        </h4>
+                    </div>
+
+                    <div className="flex flex-row items-start mb-5 border-b border-[#E2E7EE]">
+                        <button>
+                            <h2
+                                className={`text-[18] font-regular mb-3 mr-14 border-b-2 border-[#FFA101] font-bold`}
+                            >
+                                Harga
+                            </h2>
+                        </button>
+                    </div>
+
+                    <div className="bg-[#000000]flex flex-row relative rounded-full overflow-hidden mb-5">
+                        <input
+                            className="w-full md:w-auto h-[40px] md:h-[50px] pl-12 pr-4 py-2 md:py-3 bg-white border border-gray-300 rounded-full text-[16px] md:text-[20px] font-bold outline-none"
+                            type="text"
+                            placeholder="Cari User"
+                        />
+
+                        <button
+                            className="bg-blue-500 h-[40px] md:h-[50px] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ml-8"
+                            onClick={() => handlePage("/harga/create")}
+                        >
+                            Add Data
+                        </button>
+                    </div>
+
+                    <div className="flex flex-wrap overflow-hidden rounded-lg shadow-lg">
+                        <div className="min-w-full divide-y divide-gray-200 rounded-lg overflow-hidden">
+                            <div className="flex">
+                                <h1 className="px-6 py-3 bg-[#B9B9B9] text-center text-xs leading-4 font-medium text-black uppercase tracking-wider">
+                                    ID
+                                </h1>
+                                <h1 className="px-6 py-3 bg-[#B9B9B9] text-center text-xs leading-4 font-medium text-black uppercase tracking-wider w-[200px]">
+                                    ID_Fasilitas
+                                </h1>
+                                <h1 className="px-6 py-3 bg-[#B9B9B9] text-center text-xs leading-4 font-medium text-black uppercase tracking-wider w-[200px]">
+                                    Nama
+                                </h1>
+                                <h1 className="px-6 py-3 bg-[#B9B9B9] text-center text-xs leading-4 font-medium text-black uppercase tracking-wider w-[200px]">
+                                    Harga
+                                </h1>
+                                <h1 className="px-6 py-3 bg-[#B9B9B9] text-center text-xs leading-4 font-medium text-black uppercase tracking-wider w-[200px]">
+                                    Action
+                                </h1>
+                            </div>
+
+                            <div className="bg-white divide-y divide-gray-200">
+                                <div className="">
+                                    {dataharga.map((data, index) => (
+                                        <div className="flex" key={index}>
+                                            <div className="px-6 py-4 whitespace-no-wrap">
+                                                {data.id}
+                                            </div>
+                                            <div className="px-6 py-4 whitespace-no-wrap w-[200px]">
+                                            {data.id_fasilitas}
+                                            </div>
+                                            <div className="px-6 py-4 break-all w-[200px]">
+                                                {data.nama}
+                                            </div>
+                                            <div className="px-6 py-4 break-all w-[200px]">
+                                                {data.harga}
+                                            </div>
+                                            <div className="px-6 py-4 whitespace-no-wrap flex items-center justify-center w-[200px]">
+                                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-2">
+                                                    Edit
+                                                </button>
+                                                <button
+                                                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
+                                                    onClick={() =>
+                                                        handleDelete(
+                                                            data.id
+                                                        )
+                                                    }
+                                                >
+                                                    Delete
+                                                </button>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
