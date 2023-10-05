7cd92c58fa44326c0cceb2bc19e7d9d4e9bcecaf
Alfian Dorif Murtadlo1234 <100700571+IndyJones123@users.noreply.github.com>
2023-10-05 20:33:00 +0700

    Edit Misc

--- src/pages/miscellaneous/edit.tsx
+++ src/pages/miscellaneous/edit.tsx
@@ -4,10 +4,11 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 
 interface misc {
+    id_misc : number;
     nama_instansi: string;
-    logo_instansi: number;
+    logo_instansi: string;
     no_hp : string;
-    email: number;
+    email: string;
     instagram: string;
     laman_web: string;
     nama_pic: string;
@@ -19,6 +20,17 @@ export default function Miscellaneous() {
     const router = useRouter();
 
     const [dataMisc, setDataMisc] = useState<misc>();
+    //data input Misc
+    const [id_misc, setId_Misc] = useState<number>();
+    const [nama_instansi, setNama_Instansi] = useState<string>("");
+    const [no_hp, setNo_Hp] = useState<string>("");
+    const [logo_instansi, setLogo_Instansi] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [instagram, setInstagram] = useState<string>("");
+    const [laman_web, setLaman_Web] = useState<string>("");
+    const [nama_pic, setNama_Pic] = useState<string>("");
+    const [nip_pic, setNip_Pic] = useState<string>("");
+    const [tanda_tangan, setTanda_Tangan] = useState<string>("");
 
     async function getDataMisc() {
         try {
@@ -31,22 +43,101 @@ export default function Miscellaneous() {
         }
     }
 
+    async function updateDataMisc
+    (
+        nama_instansi : string,
+        no_hp : string,
+        email : string,
+        instagram : string,
+        laman_web : string,
+        nama_pic : string,
+        nip_pic : string,
+        tanda_tangan : string,
+        logo_instansi : string
+    ) 
+    {
+        try {
+            const res = await fetch(
+                `https://api.ricogann.com/api/misc/${id_misc}`,
+                {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        nama_instansi:  nama_instansi,
+                        no_hp : no_hp,
+                        email : email,
+                        instagram : instagram,
+                        laman_web : laman_web,
+                        nama_pic : nama_pic,
+                        nip_pic : nip_pic,
+                        tanda_tangan : tanda_tangan,
+                        logo_instansi : logo_instansi
+                    }),
+                }
+            );
+
+            const data = await res.json();
+            router.reload();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
+    const handleDataMisc = (
+    ) => {
+        updateDataMisc(nama_instansi, no_hp, email, instagram, laman_web, nama_pic, nip_pic, tanda_tangan, logo_instansi );
+    };
+
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.name === "Nama_Instansi") {
+            setNama_Instansi(e.target.value);
+        } else if (e.target.name === "Email") {
+            setEmail(e.target.value);
+        } else if (e.target.name === "Instagram") {
+            setInstagram(e.target.value);
+        } else if (e.target.name === "No_Hp") {
+            setNo_Hp(e.target.value);
+        } else if (e.target.name === "Laman_Web") {
+            setLaman_Web(e.target.value);
+        } else if (e.target.name === "Nama_Pic") {
+            setNama_Pic(e.target.value);
+        } else if (e.target.name === "Nip_Pic") {
+            setNip_Pic(e.target.value);
+        } else if (e.target.name === "Tanda_Tangan") {
+            setTanda_Tangan(e.target.value);
+        } else if (e.target.name === "Logo_Instansi") {
+            setLogo_Instansi(e.target.value);
+        } 
+        
+    };
+
+
     useEffect(() => {
         async function fetchData() {
             try {
                 const dataMisc = await getDataMisc();
 
-                setDataMisc(dataMisc.data);
-
+                setNama_Instansi(dataMisc.data.nama_instansi);
+                setEmail(dataMisc.data.email);
+                setInstagram(dataMisc.data.instagram);
+                setLaman_Web(dataMisc.data.laman_web);
+                setLogo_Instansi(dataMisc.data.logo_instansi);
+                setNama_Pic(dataMisc.data.nama_pic);
+                setNip_Pic(dataMisc.data.nip_pic);
+                setNo_Hp(dataMisc.data.no_hp);
+                setTanda_Tangan(dataMisc.data.tanda_tangan)
 
             } catch (error) {
                 console.error("error fetching data fasilitas ", error);
             }
         }
         fetchData()
-    });
+    },[]);
     
     console.log(dataMisc);
+    console.log(nama_instansi);
 
     return (
         <div className="flex  bg-[#FFFFFF] overflow-x-hidden">
@@ -78,57 +169,99 @@ export default function Miscellaneous() {
                                 <h1 className="flex-auto px-6 py-3  text-left text-xs leading-4 font-medium text-black uppercase tracking-wider ">
                                     Nama Instansi
                                 </h1>
-                                <h1 className="px-6 py-3 text-left text-xs leading-4 font-medium text-black uppercase tracking-wider">
-                                    {dataMisc?.nama_instansi}
-                                </h1>
+                                <input 
+                                type="string" 
+                                className="px-6 py-3 text-left text-xs leading-4 font-medium text-black uppercase tracking-wider w-[400px] bg-[#FFFFFF] rounded-lg "
+                                name="Nama_Instansi"
+                                value={nama_instansi}
+                                onChange={handleInput}
+                                >
+                                    
+                                </input>
                                 </div>
                                 <div className="flex flex-fit gap-3 bg-[#B9B9B9] rounded-lg">
                                 <h1 className="flex-auto px-6 py-3  text-left text-xs leading-4 font-medium text-black uppercase tracking-wider ">
                                     No HP BPU
                                 </h1>
-                                <h1 className="px-6 py-3 text-left text-xs leading-4 font-medium text-black uppercase tracking-wider">
-                                    {dataMisc?.no_hp}
-                                </h1>
+<input 
+                                type="string" 
+                                className="px-6 py-3 text-left text-xs leading-4 font-medium text-black uppercase tracking-wider w-[400px] bg-[#FFFFFF] rounded-lg "
+                                name="No_Hp"
+                                value={no_hp}
+                                onChange={handleInput}
+                                >
+                                    
+                                </input>
                                 </div>
                                 <div className="flex flex-fit gap-3 bg-[#B9B9B9] rounded-lg">
                                 <h1 className="flex-auto px-6 py-3  text-left text-xs leading-4 font-medium text-black uppercase tracking-wider ">
                                     NIP
                                 </h1>
-                                <h1 className="px-6 py-3 text-left text-xs leading-4 font-medium text-black uppercase tracking-wider">
-                                    {dataMisc?.nip_pic}
-                                </h1>
+<input 
+                                type="string" 
+                                className="px-6 py-3 text-left text-xs leading-4 font-medium text-black uppercase tracking-wider w-[400px] bg-[#FFFFFF] rounded-lg "
+                                name="Nip_Pic"
+                                value={nip_pic}
+                                onChange={handleInput}
+                                >
+                                    
+                                </input>
                                 </div>
                                 <div className="flex flex-fit gap-3 bg-[#B9B9B9] rounded-lg">
                                 <h1 className="flex-auto px-6 py-3  text-left text-xs leading-4 font-medium text-black uppercase tracking-wider ">
                                     Nama Pimpinan BPU
                                 </h1>
-                                <h1 className="px-6 py-3 text-left text-xs leading-4 font-medium text-black uppercase tracking-wider">
-                                    {dataMisc?.nama_pic}
-                                </h1>
+<input 
+                                type="string" 
+                                className="px-6 py-3 text-left text-xs leading-4 font-medium text-black uppercase tracking-wider w-[400px] bg-[#FFFFFF] rounded-lg "
+                                name="Nama_Pic"
+                                value={nama_pic}
+                                onChange={handleInput}
+                                >
+                                    
+                                </input>
                                 </div>
                                                                 <div className="flex flex-fit gap-3 bg-[#B9B9B9] rounded-lg">
                                 <h1 className="flex-auto px-6 py-3  text-left text-xs leading-4 font-medium text-black uppercase tracking-wider ">
                                     Instagram
                                 </h1>
-                                <h1 className="px-6 py-3 text-left text-xs leading-4 font-medium text-black uppercase tracking-wider">
-                                    {dataMisc?.instagram}
-                                </h1>
+<input 
+                                type="string" 
+                                className="px-6 py-3 text-left text-xs leading-4 font-medium text-black uppercase tracking-wider w-[400px] bg-[#FFFFFF] rounded-lg "
+                                name="Instagram"
+                                value={instagram}
+                                onChange={handleInput}
+                                >
+                                    
+                                </input>
                                 </div>
                                                                 <div className="flex flex-fit gap-3 bg-[#B9B9B9] rounded-lg">
                                 <h1 className="flex-auto px-6 py-3  text-left text-xs leading-4 font-medium text-black uppercase tracking-wider ">
                                     Website
                                 </h1>
-                                <h1 className="px-6 py-3 text-left text-xs leading-4 font-medium text-black uppercase tracking-wider">
-                                    {dataMisc?.laman_web}
-                                </h1>
+<input 
+                                type="string" 
+                                className="px-6 py-3 text-left text-xs leading-4 font-medium text-black uppercase tracking-wider w-[400px] bg-[#FFFFFF] rounded-lg "
+                                name="Laman_Web"
+                                value={laman_web}
+                                onChange={handleInput}
+                                >
+                                    
+                                </input>
                                 </div>
                                 <div className="flex flex-fit gap-3 bg-[#B9B9B9] rounded-lg">
                                 <h1 className="flex-auto px-6 py-3  text-left text-xs leading-4 font-medium text-black uppercase tracking-wider ">
                                     Email
                                 </h1>
-                                <h1 className="px-6 py-3 text-left text-xs leading-4 font-medium text-black uppercase tracking-wider">
-                                    {dataMisc?.email}
-                                </h1>
+<input 
+                                type="string" 
+                                className="px-6 py-3 text-left text-xs leading-4 font-medium text-black uppercase tracking-wider w-[400px] bg-[#FFFFFF] rounded-lg "
+                                name="Email"
+                                value={email}
+                                onChange={handleInput}
+                                >
+                                    
+                                </input>
                                 </div>
                                 <div className="flex flex-row gap-3 rounded-lg">
                                 <div className="flex flex-fit gap-3 bg-[#B9B9B9] rounded-lg">
@@ -144,10 +277,19 @@ export default function Miscellaneous() {
                                 <img className="h-[100px] bg-[#FFFFFF]" src="" alt="" />
                                 </div>
                                     </div>
-                               
+                               <input 
+                                type="string" 
+                                className="px-6 py-3 text-left text-xs leading-4 font-medium text-black uppercase tracking-wider w-[400px] bg-[#FFFFFF] rounded-lg hidden"
+                                placeholder={dataMisc?.id_misc+"......"}
+                                ></input>
 
-                                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-2">
-                                    Edit Miscellaneous
+                                 <button 
+                                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-2"
+                                 onClick={ 
+                                            handleDataMisc 
+                                        }
+                                 >
+                                    Submit
                                 </button>
                             </div>
                         </div>
